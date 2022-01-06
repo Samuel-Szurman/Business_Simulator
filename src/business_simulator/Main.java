50770880882bc83f576fc51ae37048190498f3b8
SamuelSzurman <SamuelSzurman@users.noreply.github.com>
2022-01-06 20:50:54 +0100

    adding documentation

--- src/business_simulator/Main.java
+++ src/business_simulator/Main.java
@@ -6,6 +6,11 @@ import java.io.IOException;
  * W tej klasie działa gra
  */
 public class Main{
+    /**
+     * Metoda, w której działa cały program
+     * @param args Argumenty z wiersza poleceń
+     * @throws IOException Wyjątek wywoływany w przypadku problemu z wczytaniem obrazków z plików
+     */
     public static void main(String[] args) throws IOException {
         new GameWindow();
     }
--- src/business_simulator/UpgradesContainer.java
+++ src/business_simulator/UpgradesContainer.java
@@ -8,24 +8,39 @@ import java.io.IOException;
  * Przedstawia graficznie produkt oraz pokazuje dostępne inwestycje
  */
 public class UpgradesContainer {
+    /** Szerokość całego elementu */
     private final int width = 170;
+    /** Wysokośc całego elementu */
     private final int height = 350;
+    /** Rozmiar marginesu */
     private final int margin = 10;
-
+    /** Rozmiar obrazka (szerokość i wysokość) */
     private final int imageSize = width - 2*margin;
-
+    /** Współrzędna x całego elementu */
     private final int posX;
+    /** Współrzędna y całego elementu */
     private final int posY;
-
+    /** Współrzędna x obrazka */
     private final int imageX;
+    /** Współrzędna y obrazka */
     private final int imageY;
-
+    /** Obrazek produktu */
     private final BufferedImage logo;
+    /** Tablica ulepszeń */
     private final Upgrade[] upgrades;
 
-    public UpgradesContainer(BufferedImage logo, int x, int y, ProductTab productTab, GameWindow gameWindow) throws IOException {
-        this.posX = x;
-        this.posY = y;
+    /**
+     * Jedyny konstruktor klasy UpgradeContainer
+     * @param logo Obrazek produktu
+     * @param posX Współrzędna x całego elementu
+     * @param posY Współrzędna y całego elementu
+     * @param productTab Referencja do zakładki danego produktu
+     * @param gameWindow Referencja do okna gry
+     * @throws IOException Wyjątek wywoływany w przypadku problemu z wczytaniem obrazków z plików
+     */
+    public UpgradesContainer(BufferedImage logo, int posX, int posY, ProductTab productTab, GameWindow gameWindow) throws IOException {
+        this.posX = posX;
+        this.posY = posY;
         this.imageX = posX + margin;
         this.imageY = posY + margin;
 
@@ -40,6 +55,11 @@ public class UpgradesContainer {
         upgrades[1] = new ProductionPriceUpgrade("Zmniejsz koszt produkcji", optionX, optionY + 60, optionWidth, productTab, gameWindow);
         upgrades[2] = new QualityUpgrade("Zwiększ jakość produktu", optionX, optionY + 120, optionWidth, productTab, gameWindow);
     }
+
+    /**
+     * Metoda rysuje cały element na ekranie
+     * @param g Obiekt klasy Graphics, na którym jest rysowany element
+     */
     public void draw(Graphics g){
         g.setColor(Color.GRAY);
         g.fillRoundRect(posX, posY, width, height, 10, 10);
@@ -50,6 +70,11 @@ public class UpgradesContainer {
         }
     }
 
+    /**
+     * Metoda wywoływana przy kliknięciu myszką
+     * @param x Współrzędna x myszki
+     * @param y Współrzędna y myszki
+     */
     public void clicked(int x, int y){
         for(Upgrade upgrade : upgrades){
             upgrade.clicked(x, y);
